ae9b6d368988a90ed18a2a7257992e8432e4c89c
eyramds <eyram.souza@gmail.com>
2022-06-28 21:32:36 +0200

    exchange controller in progress

---
+++ api/src/Controller/ExchangeRefuseController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Game;
+use App\Entity\Exchange;
+use App\lib\IgdbBundle\IgdbWrapper\IgdbWrapper;
+
+class ExchangeRefuseController{
+
+    private $igdb;
+
+    public function __construct(IgdbWrapper $igdb){
+        $this->igdb = $igdb;
+    }
+
+    public function __invoke(Exchange $data): Exchange {
+
+        if($data->getConfirmed() !== null){
+            return $this->redirectToRoute('home');
+        }
+        $data->setConfirmed(false);
+
+        $messageConfirmReceiver = (new \Swift_Message('Votre refus de swap a bien été prise en compte !'))
+            ->setFrom('swapit.esgi@gmail.com')
+            ->setTo($data->getUserOwner()->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'mail/swap_refused_to_owner.html.twig',
+                    [
+                        'exchange' => $data,
+                        'user' => $data->getUserProposer(),
+                        'ownerGame' => $data->getOwnerGame(),
+                        'selected_game' => $data->getGame(),
+                        'owner' => $data->getUserOwner()
+                    ]
+                    ),
+                    'text/html'
+                );
+        $mailer->send($messageConfirmReceiver);
+
+        $messageConfirmProposer = (new \Swift_Message('Votre demande de swap a été refusée ! :('))
+            ->setFrom('swapit.esgi@gmail.com')
+            ->setTo($data->getUserProposer()->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'mail/swap_refused_to_proposer.html.twig',
+                    [
+                        'exchange' => $data,
+                        'user' => $data->getUserProposer(),
+                        'ownerGame' => $data->getOwnerGame(),
+                        'selected_game' => $data->getGame(),
+                        'owner' => $data->getUserOwner()
+                    ]
+                    ),
+                    'text/html'
+                );
+                
+        $mailer->send($messageConfirmProposer);
+
+        return $data;
+    }
+}
---
+++ api/src/Security/ExchangeVoter.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Security;
+
+use App\Entity\Exchange;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class ExchangeVoter extends Voter
+{
+    // these strings are just invented: you can use anything
+    const VIEW = 'view';
+    const EDIT = 'edit';
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        // if the attribute isn't one we support, return false
+        if (!in_array($attribute, [self::VIEW, self::EDIT])) {
+            return false;
+        }
+
+        // only vote on `Exchange` objects
+        if (!$subject instanceof Exchange) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            // the user must be logged in; if not, deny access
+            return false;
+        }
+
+        // you know $subject is a Exchange object, thanks to `supports()`
+        /** @var Exchange $Exchange */
+        $Exchange = $subject;
+
+        switch ($attribute) {
+            case self::VIEW:
+                return $this->canView($Exchange, $user);
+            case self::EDIT:
+                return $this->canEdit($Exchange, $user);
+        }
+
+        throw new \LogicException('This code should not be reached!');
+    }
+
+    private function canView(Exchange $Exchange, User $user): bool
+    {
+        // if they can edit, they can view
+        if ($this->canEdit($Exchange, $user)) {
+            return true;
+        }
+
+        // the Exchange object could have, for example, a method `isPrivate()`
+        return ($Exchange->getUserOwner() == $user || $Exchange->getUserProposer() == $user );
+    }
+
+    private function canEdit(Exchange $Exchange, User $user): bool
+    {
+        // this assumes that the Exchange object has a `getOwner()` method
+        return $user === $Exchange->getUserOwner();
+    }
+}
