205139d2bd307a9f4848a418aed61b34d8cba5bc
Nassim95 <nasfahdine@outlook.com>
2022-06-29 13:17:03 +0200

    fixing exchange

--- api/src/Controller/ExchangeConfirmController.php
+++ api/src/Controller/ExchangeConfirmController.php
@@ -9,54 +9,54 @@ class ExchangeConfirmController{
 
     private $igdb;
 
-    public function __construct(IgdbWrapper $igdb){
-        $this->igdb = $igdb;
-    }
+    // public function __construct(IgdbWrapper $igdb){
+    //     $this->igdb = $igdb;
+    // }
 
-    public function __invoke(Exchange $data): Exchange {
-        if($data->getConfirmed() !== null){
-            return $this->redirectToRoute('home');
-        }
-        $data->setConfirmed(true);
+    // public function __invoke(Exchange $data): Exchange {
+        // if($data->getConfirmed() !== null){
+        //     return $this->redirectToRoute('home');
+        // }
+        // $data->setConfirmed(true);
 
-        $messageConfirmReceiver = (new \Swift_Message('Votre confirmation de swap a bien été prise en compte !'))
-            ->setFrom('swapit.esgi@gmail.com')
-            ->setTo($data->getUserOwner()->getEmail())
-            ->setBody(
-                $this->renderView(
-                    'mail/swap_confirmation_to_owner.html.twig',
-                    [
-                        'exchange' => $data,
-                        'user' => $data->getUserProposer(),
-                        'ownerGame' => $data->getOwnerGame(),
-                        'selected_game' => $data->getGame(),
-                        'owner' => $data->getUserOwner()
-                    ]
-                    ),
-                    'text/html'
-                );
+        // $messageConfirmReceiver = (new \Swift_Message('Votre confirmation de swap a bien été prise en compte !'))
+        //     ->setFrom('swapit.esgi@gmail.com')
+        //     ->setTo($data->getUserOwner()->getEmail())
+        //     ->setBody(
+        //         $this->renderView(
+        //             'mail/swap_confirmation_to_owner.html.twig',
+        //             [
+        //                 'exchange' => $data,
+        //                 'user' => $data->getUserProposer(),
+        //                 'ownerGame' => $data->getOwnerGame(),
+        //                 'selected_game' => $data->getGame(),
+        //                 'owner' => $data->getUserOwner()
+        //             ]
+        //             ),
+        //             'text/html'
+        //         );
                 
-        $mailer->send($messageConfirmReceiver);
+        // $mailer->send($messageConfirmReceiver);
 
-        $messageConfirmProposer = (new \Swift_Message('Votre demande de swap a bien été validée !'))
-            ->setFrom('swapit.esgi@gmail.com')
-            ->setTo($data->getUserProposer()->getEmail())
-            ->setBody(
-                $this->renderView(
-                    'mail/swap_confirmation_to_proposer.html.twig',
-                    [
-                        'exchange' => $data,
-                        'user' => $data->getUserProposer(),
-                        'ownerGame' => $data->getOwnerGame(),
-                        'selected_game' => $data->getGame(),
-                        'owner' => $data->getUserOwner()
-                    ]
-                    ),
-                    'text/html'
-                );
+        // $messageConfirmProposer = (new \Swift_Message('Votre demande de swap a bien été validée !'))
+        //     ->setFrom('swapit.esgi@gmail.com')
+        //     ->setTo($data->getUserProposer()->getEmail())
+        //     ->setBody(
+        //         $this->renderView(
+        //             'mail/swap_confirmation_to_proposer.html.twig',
+        //             [
+        //                 'exchange' => $data,
+        //                 'user' => $data->getUserProposer(),
+        //                 'ownerGame' => $data->getOwnerGame(),
+        //                 'selected_game' => $data->getGame(),
+        //                 'owner' => $data->getUserOwner()
+        //             ]
+        //             ),
+        //             'text/html'
+        //         );
                 
-        $mailer->send($messageConfirmProposer);
+        // $mailer->send($messageConfirmProposer);
     
-        return $data;
-    }
+        // return $data;
+    // }
 }
--- api/src/Controller/ExchangeRefuseController.php
+++ api/src/Controller/ExchangeRefuseController.php
@@ -10,54 +10,54 @@ class ExchangeRefuseController{
 
     private $igdb;
 
-    public function __construct(IgdbWrapper $igdb){
-        $this->igdb = $igdb;
-    }
+    // public function __construct(IgdbWrapper $igdb){
+    //     $this->igdb = $igdb;
+    // }
 
-    public function __invoke(Exchange $data): Exchange {
+    // public function __invoke(Exchange $data): Exchange {
 
-        if($data->getConfirmed() !== null){
-            return $this->redirectToRoute('home');
-        }
-        $data->setConfirmed(false);
+    //     if($data->getConfirmed() !== null){
+    //         return $this->redirectToRoute('home');
+    //     }
+    //     $data->setConfirmed(false);
 
-        $messageConfirmReceiver = (new \Swift_Message('Votre refus de swap a bien été prise en compte !'))
-            ->setFrom('swapit.esgi@gmail.com')
-            ->setTo($data->getUserOwner()->getEmail())
-            ->setBody(
-                $this->renderView(
-                    'mail/swap_refused_to_owner.html.twig',
-                    [
-                        'exchange' => $data,
-                        'user' => $data->getUserProposer(),
-                        'ownerGame' => $data->getOwnerGame(),
-                        'selected_game' => $data->getGame(),
-                        'owner' => $data->getUserOwner()
-                    ]
-                    ),
-                    'text/html'
-                );
-        $mailer->send($messageConfirmReceiver);
+    //     $messageConfirmReceiver = (new \Swift_Message('Votre refus de swap a bien été prise en compte !'))
+    //         ->setFrom('swapit.esgi@gmail.com')
+    //         ->setTo($data->getUserOwner()->getEmail())
+    //         ->setBody(
+    //             $this->renderView(
+    //                 'mail/swap_refused_to_owner.html.twig',
+    //                 [
+    //                     'exchange' => $data,
+    //                     'user' => $data->getUserProposer(),
+    //                     'ownerGame' => $data->getOwnerGame(),
+    //                     'selected_game' => $data->getGame(),
+    //                     'owner' => $data->getUserOwner()
+    //                 ]
+    //                 ),
+    //                 'text/html'
+    //             );
+    //     $mailer->send($messageConfirmReceiver);
 
-        $messageConfirmProposer = (new \Swift_Message('Votre demande de swap a été refusée ! :('))
-            ->setFrom('swapit.esgi@gmail.com')
-            ->setTo($data->getUserProposer()->getEmail())
-            ->setBody(
-                $this->renderView(
-                    'mail/swap_refused_to_proposer.html.twig',
-                    [
-                        'exchange' => $data,
-                        'user' => $data->getUserProposer(),
-                        'ownerGame' => $data->getOwnerGame(),
-                        'selected_game' => $data->getGame(),
-                        'owner' => $data->getUserOwner()
-                    ]
-                    ),
-                    'text/html'
-                );
+    //     $messageConfirmProposer = (new \Swift_Message('Votre demande de swap a été refusée ! :('))
+    //         ->setFrom('swapit.esgi@gmail.com')
+    //         ->setTo($data->getUserProposer()->getEmail())
+    //         ->setBody(
+    //             $this->renderView(
+    //                 'mail/swap_refused_to_proposer.html.twig',
+    //                 [
+    //                     'exchange' => $data,
+    //                     'user' => $data->getUserProposer(),
+    //                     'ownerGame' => $data->getOwnerGame(),
+    //                     'selected_game' => $data->getGame(),
+    //                     'owner' => $data->getUserOwner()
+    //                 ]
+    //                 ),
+    //                 'text/html'
+    //             );
                 
-        $mailer->send($messageConfirmProposer);
+    //     $mailer->send($messageConfirmProposer);
 
-        return $data;
-    }
+        // return $data;
+    // }
 }
