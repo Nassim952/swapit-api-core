c78dd5a706bee7b37a25e2a8a11d23062fb3519a
nassim95 <nasfahdine@gmail.com>
2022-07-06 13:08:55 +0200

    https

--- public/index.php
+++ public/index.php
@@ -2,10 +2,8 @@
 
 use App\Kernel;
 
-$_SERVER['HTTPS'] = 'on';
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($_SERVER['HTTPS'] = 'on', $context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
