f7b96d0ad91a503912c588623753817437f23a3a
nassim95 <nasfahdine@gmail.com>
2022-07-06 15:02:30 +0200

    https fixed

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 
 use App\Kernel;
 
-$_SERVER['HTTP_X_FORWARDED_PROTO'] = $_SERVER['HTTP_CUSTOM_FORWARDED_PROTO'];
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
