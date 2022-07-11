9728440ff94d3f02defd6973aa6e897c049a5dec
Randy Mb <randymb77@gmail.com>
2022-07-11 18:51:58 +0200

    Post User - done & fix Post Exchange - started

--- api/src/DataPersister/ExchangeDataPersister.php
+++ api/src/DataPersister/ExchangeDataPersister.php
@@ -4,13 +4,15 @@ namespace App\DataPersister;
 use Doctrine\ORM\EntityManagerInterface;
 use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
 use App\Entity\Exchange;
+use Symfony\Component\Security\Core\Security;
 
 class ExchangeDataPersister implements ContextAwareDataPersisterInterface {
 
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager) {
+    public function __construct(EntityManagerInterface $entityManager, Security $security) {
         $this->entityManager = $entityManager;
+        $this->security = $security;
     }
 
     public function supports($data, array $context = []) : bool
@@ -21,14 +23,21 @@ class ExchangeDataPersister implements ContextAwareDataPersisterInterface {
 
     public function persist($data, array $context = [])
     {
-        $data->setOwner($data->getOwner());
-        $data->setProposer($data->getProposer());
-        $data->setProposerGame($data->getProposerGame());
-        $data->setSenderGame($data->getSenderGame());
+        // $data->setOwner($data->getOwner());
+        // $data->setProposer($data->getProposer());
+        // $data->setProposerGame($data->getProposerGame());
+        // $data->setSenderGame($data->getSenderGame());
         // $data->setConfirmed(null);
 
-        $this->entityManager->persist($data);
-        $this->entityManager->flush();
+        $user=$this->security->getUser();
+        if($data->getProposer() == $user){
+            $this->entityManager->persist($data);
+            $this->entityManager->flush();
+        } 
+        
+        // elseif ($data->getOwner($user)) {
+            
+        // }
     }
 
     public function remove($data, array $context = [])
